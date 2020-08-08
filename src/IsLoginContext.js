8784f08cd707ef56afd61c2a39507954d21b8dcb
vincentchandra <vincentchristian6@gmail.com>
2020-08-09 00:11:01 +0700

    mengganti status isLogin menjadi false

--- src/IsLoginContext.js
+++ src/IsLoginContext.js
@@ -2,7 +2,7 @@ import React, {useState, createContext} from 'react';
 
 export const IsLoginContext = createContext()
 export const IsLoginProvider = props => {
-    const [isLogin, setIsLogin]  =  useState(true)
+    const [isLogin, setIsLogin]  =  useState(false)
     return(
         <IsLoginContext.Provider value = {[isLogin,setIsLogin]}>
             {props.children}
